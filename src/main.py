9bcf1964ff9e3b7854aa1a29d5126ef0f593e26e
Creatix <creatixchu@gmail.com>
2023-04-04 22:16:17 +0800

    ver 1.0

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import sys
+import sympy as sy
 import numpy as np
+from numpy import sin, cos, log
 from spl.spl_train import run_spl
 from mksr.combiner import combiner
 
@@ -7,7 +9,8 @@ np.random.seed(1)
 output_folder = 'logs/'   ## directory to save discovered results
 save_eqs = True           ## if true, discovered equations are saved to "output_folder" dir
 
-target_equ = "x0**2 * x1 + x0 + 2 * x1"
+# target_equ = "x0**2 * x1 + x0 + 2 * x1"
+target_equ = "sin(x0) * x1 + 3"
 var_num = 2
 x_range = (-5, 5)
 task = 'task1'
@@ -15,12 +18,17 @@ grammars = ['A->A+A', 'A->A-A', 'A->A*A', 'A->A/A',
             'A->x', 'A->C', 
             'A->exp(A)', 'A->cos(x)', 'A->sin(x)']
 nt_nodes = ['A']
-pivot = [3.14, 0.618]
+pivot = [0.233, 0.618]
 bst_eqs = []
 
-test_mksr_only = 1
+test_mksr_only = 0
 if test_mksr_only:
-    bst_eqs = ['0.618*x0**2 + x0 + 1.236', '11.8596*x1 + 3.14']
+    # bst_eqs = [sy.simplify('0.618*x0**2 + x0 + 1.236'), sy.simplify('11.8596*x1 + 3.14')]
+    bst_eqs = ['0.618*sin(x0) + 3.0', '0.2309*x1 + 3.0']
+    cb = combiner(var_num = var_num,
+                  pivot = pivot,
+                  eqs = bst_eqs)
+    print(cb.equation)
     exit(0)
 
 
@@ -44,7 +52,13 @@ for var_id in range(var_num):
                                             train_sample = train_sample,
                                             test_sample = test_sample,
                                             transplant_step = 200,
-                                            num_transplant = 2)
+                                            num_transplant = 2,
+                                            eta = 0.999)
     bst_eqs.append(max(all_eqs, key=lambda x: x[1])[0].replace('x', f'x{var_id}'))
 
-print(bst_eqs)                                    
+print(bst_eqs)
+
+cb = combiner(var_num = var_num,
+                pivot = pivot,
+                eqs = bst_eqs)
+print(cb.equation)                  
